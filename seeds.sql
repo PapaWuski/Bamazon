00addd6a6a261e48332c5cf090252c520ed0d529
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-10-03 19:13:46 -0700

    moved seeds out of schema into seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE bamazon;
+INSERT INTO products
+    (product_name,department_name,price,stock_quantity)
+VALUES
+    ("cheese", "food", 5, 100),
+    ("beer", "food", 3.51, 100),
+    ("tennis balls", "sports", 1, 100),
+    ("beef", "food", 4, 100),
+    ("personal computer", "electronics", 1000, 5),
+    ("macbook", "electronics", 3000, 5),
+    ("xbox controller", "electronics", 30, 100),
+    ("camera", "electronics", 1500, 100),
+    ("halloween candy", "food", 1, 100),
+    ("door", "home", 100, 100),
+    ("bookcase", "home", 100, 5),
+    ("plushies", "home", 50, 500);
